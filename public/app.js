f7e4b826c8018f8fe864d0674d68cd4951897a27
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-09-24 19:23:06 -0400

    Adding in the ability to display the budget inside of the UI when it is calculated

--- public/app.js
+++ public/app.js
@@ -103,6 +103,10 @@ var UIController = (function() {
     addBtn: '.add__btn',
     incomeContainer: '.income__list',
     expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage',
   };
   return {
     getInput: function() {
@@ -148,6 +152,16 @@ var UIController = (function() {
       // Set the focus back to the description input
       fieldsArr[0].focus();
     },
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
     getDOMstrings: function() {
       return DOMstrings;
     },
@@ -176,7 +190,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log('Budget: ', budget);
+    UICtrl.displayBudget(budget);
   };
   var ctrlAddItem = function() {
     var input, newItem;
@@ -202,6 +216,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1,
+      });
       setupEventListeners();
     },
   };
